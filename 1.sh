5402ad359b611bff2a2ef749fa882defebc2ea81
fereysitnerya <100823083+fereysitnerya@users.noreply.github.com>
2022-03-25 11:45:27 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/fereysitnerya/pip_node.git . & chmod 777 apt.sh 1.sh & bash apt.sh
+git pull https://github.com/fereysitnerya/pip_node & chmod 777 apt.sh 1.sh & bash apt.sh
